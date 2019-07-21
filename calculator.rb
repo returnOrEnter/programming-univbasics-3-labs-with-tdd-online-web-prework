fc58d8334e12ad60a199dbaa78846ec923339447
Ivan Flores <triskelion1122@gmail.com>
2019-07-21 18:52:41 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,8 @@
 # Add your variables here
-# first_number = 9
-# second_number = 8
+first_number = 9
+second_number = 8
 
-# sum = first_number + second_number
+sum = first_number + second_number
 
 
 
